a158d22f0644da8abccbe6b95215b4bacb331e23
ArdaHFO <75683433+ArdaHFO@users.noreply.github.com>
2023-11-16 00:16:59 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from views import views
+
+app = Flask(__name__)
+app.register_blueprint(views, url_prefix = "/views")
+#----------------------------------------------------------------------------------------------------
+@app.route('/')
+def index():
+    return render_template('index.html')
+#----------------------------------------------------------------------------------------------------   
+@app.route('/ping')
+def ping():
+    return 'Pong'
+#----------------------------------------------------------------------------------------------------   
+@app.route('/upload')
+def upload():
+    if 'file' not in request.files:
+        return 'File not found.'
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return 'Filename is empty.'
+
+    file.save('uploads/' + file.filename)
+    return 'The file has been uploaded successfully: ' + file.filename
+#----------------------------------------------------------------------------------------------------   
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
+#----------------------------------------------------------------------------------------------------
